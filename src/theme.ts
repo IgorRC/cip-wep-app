eec20482c761d0bbe4a09639969ee0f24c42137d
igorrc <igor.ramos.cruzado.w@gmail.com>
2025-06-18 00:12:20 -0500

    feat : colors reference organization

--- src/theme.ts
+++ src/theme.ts
@@ -2,8 +2,11 @@ import { createTheme } from '@mui/material/styles';
 
 export default createTheme({
   palette: {
-    primary: { main: '#0d47a1' }, // azul oscuro
-    secondary: { main: '#ff9100' }
+    primary: { main: '#B52E2A' }, // azul oscuro
+    secondary: { main: '#BEA062',contrastText: '#1D1916' },
+    error: {
+      main: '#E31E24'  // rojo más intenso para errores
+    }
   },
   components: {
     MuiAppBar: {
